56fd31b7cd308668333ea88f7b3e748ec7ca4e06
Artur Krochin <arturkrochin@Arturs-MacBook-Pro.local>
2020-12-09 19:23:37 +0000

    lint

--- event-emitter/producer.py
+++ event-emitter/producer.py
@@ -1,12 +1,16 @@
-from aiokafka import AIOKafkaProducer
-from pydantic import BaseModel
+import uuid
 
-import utils
+from aiokafka import AIOKafkaProducer
 
 
 class AsyncKafkaProducer:
     def __init__(self, bootstrap_servers: str, request_timeout_ms: int):
-        self._producer = AIOKafkaProducer(bootstrap_servers=bootstrap_servers, request_timeout_ms=request_timeout_ms)
+        self._producer = AIOKafkaProducer(
+            bootstrap_servers=bootstrap_servers,
+            request_timeout_ms=request_timeout_ms,
+            client_id=str(uuid.uuid4()),
+            compression_type="gzip"
+        )
 
     async def start(self):
         await self._producer.start()
