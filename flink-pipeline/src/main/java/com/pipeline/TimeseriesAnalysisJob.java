94a5b8f50bef517c1fe3bab076a162f52dc7f17d
Artur Krochin <arturkrochin@Arturs-MacBook-Pro.local>
2020-12-11 16:16:03 +0000

    execute time series analysis job

--- flink-pipeline/src/main/java/com/pipeline/TimeseriesAnalysisJob.java
+++ flink-pipeline/src/main/java/com/pipeline/TimeseriesAnalysisJob.java
@@ -1,16 +1,17 @@
 package com.pipeline;
 
 import com.pipeline.models.TimeseriesReading;
-import java.time.Duration;
-import java.util.Properties;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.formats.avro.AvroDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
+import java.time.Duration;
+import java.util.Properties;
+
 public class TimeseriesAnalysisJob {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
         Properties properties = new Properties();
@@ -27,10 +28,11 @@ public class TimeseriesAnalysisJob {
         //
         // https://ci.apache.org/projects/flink/flink-docs-release-1.12/dev/event_timestamps_watermarks.html#watermark-strategies-and-the-kafka-connector
         kafkaSource.assignTimestampsAndWatermarks(
-                WatermarkStrategy.<TimeseriesReading>forBoundedOutOfOrderness(
-                                Duration.ofSeconds(20))
+                WatermarkStrategy.<TimeseriesReading>forBoundedOutOfOrderness(Duration.ofSeconds(2))
                         .withTimestampAssigner((event, timestamp) -> event.getTimestamp()));
 
         DataStream<TimeseriesReading> stream = env.addSource(kafkaSource);
+
+        env.execute(TimeseriesAnalysisJob.class.getName());
     }
 }
