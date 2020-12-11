031aeca7c3a862aad026747c48e290c08e9f3d73
Artur Krochin <arturkrochin@Arturs-MacBook-Pro.local>
2020-12-11 15:40:59 +0000

    add kafka source

--- flink-pipeline/src/main/java/com/pipeline/TimeseriesAnalysisJob.java
+++ flink-pipeline/src/main/java/com/pipeline/TimeseriesAnalysisJob.java
@@ -1,9 +1,25 @@
 package com.pipeline;
 
-// TODO: disable Kryo serialisation
-//   Use avro serialisation instead?
+import com.pipeline.models.TimeseriesReading;
+import java.util.Properties;
+import org.apache.flink.formats.avro.AvroDeserializationSchema;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
 public class TimeseriesAnalysisJob {
     public static void main(String[] args) throws Exception {
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers", "localhost:9092");
+        properties.setProperty("group.id", "test");
+
+        DataStream<TimeseriesReading> stream =
+                env.addSource(
+                        new FlinkKafkaConsumer<>(
+                                "ts-events",
+                                AvroDeserializationSchema.forSpecific(TimeseriesReading.class),
+                                properties));
     }
 }
