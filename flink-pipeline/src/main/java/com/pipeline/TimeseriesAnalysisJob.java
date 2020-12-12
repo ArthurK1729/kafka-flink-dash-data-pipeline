9e4e22bdd5d1a4a902458a7636fd9f4a868d0b31
Artur Krochin <arturkrochin@Arturs-MacBook-Pro.local>
2020-12-12 18:31:42 +0000

    refactor kafka source

--- flink-pipeline/src/main/java/com/pipeline/TimeseriesAnalysisJob.java
+++ flink-pipeline/src/main/java/com/pipeline/TimeseriesAnalysisJob.java
@@ -4,7 +4,7 @@ import com.pipeline.models.TimeseriesReading;
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
 import org.apache.flink.formats.avro.AvroDeserializationSchema;
 import org.apache.flink.streaming.api.TimeCharacteristic;
-import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.sink.PrintSinkFunction;
 import org.apache.flink.streaming.api.windowing.assigners.SlidingEventTimeWindows;
@@ -12,6 +12,7 @@ import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Properties;
 
 public class TimeseriesAnalysisJob {
@@ -19,34 +20,50 @@ public class TimeseriesAnalysisJob {
         final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime);
 
-        Properties properties = new Properties();
-        // TODO: Do something about auto-commit here
-        properties.setProperty("bootstrap.servers", "broker:29092");
-        properties.setProperty("group.id", "test");
+        var source = getKafkaSource(env).name("source");
 
-        // TODO: Kafka-partition-aware strategy?
-        //
-        // https://ci.apache.org/projects/flink/flink-docs-release-1.12/dev/event_timestamps_watermarks.html#watermark-strategies-and-the-kafka-connector
+        var stream =
+                source.assignTimestampsAndWatermarks(
+                        WatermarkStrategy.<TimeseriesReading>forBoundedOutOfOrderness(
+                                        Duration.ofSeconds(2))
+                                .withTimestampAssigner((event, timestamp) -> event.getTimestamp()));
 
-        var kafkaSource =
-                new FlinkKafkaConsumer<>(
-                                "ts-events",
-                                AvroDeserializationSchema.forSpecific(TimeseriesReading.class),
-                                properties)
-                        .assignTimestampsAndWatermarks(
-                                WatermarkStrategy.<TimeseriesReading>forBoundedOutOfOrderness(
-                                                Duration.ofSeconds(2))
-                                        .withTimestampAssigner(
-                                                (event, timestamp) -> event.getTimestamp()));
-
-        DataStream<TimeseriesReading> stream = env.addSource(kafkaSource).name("kafkaSource");
-
-        stream.windowAll(SlidingEventTimeWindows.of(Time.seconds(10), Time.seconds(1)))
+        stream.windowAll(SlidingEventTimeWindows.of(Time.seconds(10), Time.seconds(3)))
                 .process(new AverageAggregator())
                 .name("aggregatedTimeseriesReadings");
 
         stream.addSink(new PrintSinkFunction<>(false)).name("aggregationOutput");
 
         env.execute(TimeseriesAnalysisJob.class.getName());
+    }
+
+    private static DataStreamSource<TimeseriesReading> getLocalSource(
+            StreamExecutionEnvironment env) {
+        return env.fromCollection(
+                List.of(
+                        new TimeseriesReading(2.5, 12312),
+                        new TimeseriesReading(2.38, 12313),
+                        new TimeseriesReading(10.0, 12314),
+                        new TimeseriesReading(10002.334, 12315),
+                        new TimeseriesReading(8893.3, 12316),
+                        new TimeseriesReading(3.3, 12317)));
+    }
+
+    private static DataStreamSource<TimeseriesReading> getKafkaSource(
+            StreamExecutionEnvironment env) {
+        // TODO: kafka partition aware
+        // https://ci.apache.org/projects/flink/flink-docs-release-1.12/dev/event_timestamps_watermarks.html#watermark-strategies-and-the-kafka-connector
+        Properties properties = new Properties();
+        // TODO: Do something about auto-commit here
+        properties.setProperty("bootstrap.servers", "broker:29092");
+        properties.setProperty("group.id", "test");
+
+        var kafkaSource =
+                new FlinkKafkaConsumer<>(
+                        "ts-events",
+                        AvroDeserializationSchema.forSpecific(TimeseriesReading.class),
+                        properties);
+
+        return env.addSource(kafkaSource);
     }
 }
