6b753ce1f4197db5a33c2fea96c93147c4184a85
Artur Krochin <arturkrochin@Arturs-MacBook-Pro.local>
2020-12-12 22:05:44 +0000

    switch to using per-record avro writer instead of batch

--- event-emitter/utils.py
+++ event-emitter/utils.py
@@ -1,7 +1,7 @@
 from datetime import datetime, timezone
 from io import BytesIO
 
-from fastavro import writer
+from fastavro import schemaless_writer
 from pydantic.main import BaseModel
 
 
@@ -10,11 +10,11 @@ def string_to_binary(string: str) -> bytes:
 
 
 def generate_current_epoch_time() -> int:
-    return int(datetime.now(tz=timezone.utc).timestamp() * 1000)
+    return int(datetime.now(tz=timezone.utc).timestamp())
 
 
 def model_to_bytes(event: BaseModel, avro_schema: dict) -> bytes:
     stream = BytesIO()
-    writer(stream, avro_schema, [event.dict()])
+    schemaless_writer(stream, avro_schema, event.dict())
 
     return stream.getvalue()
